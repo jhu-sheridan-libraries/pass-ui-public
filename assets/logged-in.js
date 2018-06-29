68b8f3392f8038ffab85aa3002be2faeee2bb8df
Karen Hanson <karenhanson77@gmail.com>
2018-06-29 10:12:37 -0400

    Change page title to "PASS"

--- assets/logged-in.js
+++ assets/logged-in.js
@@ -1,7 +1,7 @@
   $(document).ready(function() {
     $.ajax("/app/", {
       success: function(data) {
-        if (data.indexOf("<title>PassEmber</title>")>0) {
+        if (data.indexOf("<title>PASS</title>")>0) {
           var ul = document.getElementsByClassName("navbar-nav")[0].getElementsByTagName("li")[0];
           $(ul).after('<li class="nav-item"><a href="/app/submissions" class="nav-link ember-view">Submissions</a></li>');
           $(ul).after('<li class="nav-item"><a href="/app/grants" class="nav-link ember-view">Grants</a></li>');
